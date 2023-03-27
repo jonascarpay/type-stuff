cadc0d5b235e9947d321304353cc5c0fc14e2fb6
Jonas Carpay <jonascarpay@gmail.com>
2023-03-27 19:01:02 +0900

    Remove Lib.Bind

--- src/Lib/Telescope.hs
+++ src/Lib/Telescope.hs
@@ -3,11 +3,8 @@
 
 module Lib.Telescope where
 
-import Control.Monad.State
-import Data.Map (Map)
 import Data.Proxy
-import Data.Void
-import Lib.Bind
+import Rebound
 
 data Telescope f g a
   = Scope (f a) (Telescope f g (Bind1 a))
@@ -19,12 +16,3 @@ type PTelescope = Telescope Proxy
 flatten :: Telescope f (Telescope f g) a -> Telescope f g a
 flatten (Scope f k) = Scope f (flatten k)
 flatten (Nil g) = g
-
--- run :: (forall k. Fresh k a (f a)) -> Telescope Proxy f a
--- run _ = undefined
-
-captureMany :: f (Either Int a) -> Telescope Proxy f a
-captureMany = undefined
-  where
-    go :: (a -> r) -> (forall g. g r -> Telescope Proxy g a) -> Map Int r -> f r -> ()
-    go _ _ _ = undefined
